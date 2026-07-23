218dc97c2e3bd970839192bafbcd68e9ebcb906e
rd51 <rd51@users.noreply.github.com>
2026-07-23 15:12:40 +0400

    Fix deployment upload path

--- MLOps/mlops/hosting/hosting.py
+++ MLOps/mlops/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="mlops/deployment",     # the local folder containing your files
+    folder_path="MLOps/mlops/deployment",     # the local folder containing your files
     repo_id="RAK05/customer-churn",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
